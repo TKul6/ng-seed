0b2fb524f617073cbb784f0b09c2892b65c00321
TKul6 <123456>
2018-08-01 22:27:34 +0300

    Coloring the form-field in different color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatListModule, MatFormFieldModule, MatInputModule} from '@angular/material';
-
+import {OverlayContainer} from '@angular/cdk/overlay';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -20,4 +20,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(overlayContainer: OverlayContainer) {
+    overlayContainer.getContainerElement().classList.add('inner-theme');
+  }
+
+}
