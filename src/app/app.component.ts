a70ad032e7e04a23a290b1fdb4251a57dc124b1f
TKul6 <123456>
2018-09-30 18:55:04 +0300

    add shadow effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,14 +18,24 @@ constructor(private santizier: DomSanitizer) {
   public generateImage(text: string): void {
     const canvas: any = document.createElement('canvas');
     const context = canvas.getContext('2d');
-    canvas.height = 48;
-    canvas.width = context.measureText(text).width + 32;
-    console.log(context.measureText(text));
+    canvas.height = 50;
+    canvas.width = context.measureText(text).width + 34;
     context.fillStyle = '#ffffcc';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = '#000000';
-    context.font = '13px Roboto';
-    context.fillText(text, 16, canvas.height /2 + 4);
+    context.shadowColor = '#000000';
+    context.shadowBlur = 0;
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2.3; 
+    context.fillRect(0, 0, canvas.width -2, canvas.height -2);
+
+    const textContext = canvas.getContext('2d');
+
+    textContext.fillStyle = '#000000';
+    textContext.font = '13px Roboto';
+    textContext.shadowColor = '#000000';
+    textContext.shadowBlur = 0;
+    textContext.shadowOffsetX = 0;
+    textContext.shadowOffsetY = 0; 
+    textContext.fillText(text, 16, canvas.height /2 + 4);
     this.dataUrl = canvas.toDataURL('image/png');
     this.safeImageUrl = this.santizier.bypassSecurityTrustUrl(this.dataUrl);
   }
