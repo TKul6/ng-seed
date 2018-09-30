b98e88c886091b0ccc6fada11d1791ae0e886fd7
TKul6 <123456>
2018-09-30 18:03:38 +0300

    making a canvas image a drag image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '../../node_modules/@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  dataUrl = '';
+  safeImageUrl = null;
+
+constructor(private santizier: DomSanitizer) {
+
+}
+
+  public generateImage(text: string): void {
+    const canvas: any = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    context.fillText(text, 30, 30);
+    this.dataUrl = canvas.toDataURL('image/png');
+    this.safeImageUrl = this.santizier.bypassSecurityTrustUrl(this.dataUrl);
+  }
+
+  public addImage(event): void {
+
+console.log('setting drag image');
+const image: any = document.createElement('img');
+image.src = this.dataUrl;
+console.log(image);
+event.dataTransfer.setDragImage(image, 10, 10);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatListModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatListModule, MatFormFieldModule, MatInputModule, MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -15,7 +15,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatListModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
