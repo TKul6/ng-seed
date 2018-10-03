b068caf4f4bfd27d0c7bd6ad3774bbc110e9537d
tkhalili <tkhalili@verint.com>
2018-10-03 18:44:46 +0300

    Fix shadow p1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,42 +11,43 @@ export class AppComponent {
   dataUrl = '';
   safeImageUrl = null;
 
-constructor(private santizier: DomSanitizer) {
+  constructor(private santizier: DomSanitizer) {
 
-}
+  }
 
   public generateImage(text: string): void {
     const canvas: any = document.createElement('canvas');
     const context = canvas.getContext('2d');
-    canvas.height = 50;
-    canvas.width = context.measureText(text).width + 34;
+    canvas.height = 45;
+    context.font = "13px Arial";
+    canvas.width = context.measureText(text).width + 80;
     context.fillStyle = '#ffffcc';
-    context.shadowColor = '#000000';
-    context.shadowBlur = 0;
+    context.shadowColor = '#b8b8b8';
+    context.shadowBlur = 50;
     context.shadowOffsetX = 2;
-    context.shadowOffsetY = 2.3; 
-    context.fillRect(0, 0, canvas.width -2, canvas.height -2);
+    context.shadowOffsetY = 2.3;
+    context.fillRect(0, 0, canvas.width - 10, canvas.height - 10);
 
     const textContext = canvas.getContext('2d');
 
     textContext.fillStyle = '#000000';
-    textContext.font = '13px Roboto';
+    textContext.font = "13px Arial";
     textContext.shadowColor = '#000000';
     textContext.shadowBlur = 0;
     textContext.shadowOffsetX = 0;
-    textContext.shadowOffsetY = 0; 
-    textContext.fillText(text, 16, canvas.height /2 + 4);
+    textContext.shadowOffsetY = 0;
+    textContext.fillText(text, 16, canvas.height / 2 + 4);
     this.dataUrl = canvas.toDataURL('image/png');
     this.safeImageUrl = this.santizier.bypassSecurityTrustUrl(this.dataUrl);
   }
 
   public addImage(event): void {
 
-console.log('setting drag image');
-const image: any = document.createElement('img');
-image.src = this.dataUrl;
-console.log(image);
-event.dataTransfer.setDragImage(image, -1, -1);
+    console.log('setting drag image');
+    const image: any = document.createElement('img');
+    image.src = this.dataUrl;
+    console.log(image);
+    event.dataTransfer.setDragImage(image, -1, -1);
 
   }
 }
